a4d705a3b1e0896dde8797b2f736addf774ccb16
Ujjwal Singh <ujjwalsingh6386@gmail.com>
2022-07-02 12:37:30 +0530

    Add procfile and other updates

--- app.js
+++ app.js
@@ -199,11 +199,15 @@ List.findOne({name:routeName}, function(err,foundList){
 
 
 
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+// app.listen(port);
 
 
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started successfully");
 });
