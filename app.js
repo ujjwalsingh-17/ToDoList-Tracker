b180333f4ee3cfc5ceb077a937068b56fd7e1be1
Ujjwal Singh <ujjwalsingh6386@gmail.com>
2022-08-15 21:22:14 +0530

    changes made

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -12,29 +11,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-ujjwal:Testing123@cluster0.lwg07wr.mongodb.net/todolistDB",{useNewUrlParser: true});
 
+//Connections
+
+mongoose.connect("mongdb+srv://admin-ujjwal:Testing123@cluster0.lwg07wr.mongodb.net/todolistDB",{useNewUrlParser: true});
+
+
+//Schema
 
 const itemsSchema={
   name: String
 }
 
 const Item= mongoose.model("Item",itemsSchema)
-
-const item1= new Item({
-  name: "Have breakfast at 9:00 a.m. sharp"
-})
-
-const item2=new Item({
-  name: "Solve some questions on dsa"
-})
-
-const item3= new Item({
-  name: "Do some Web Development"
-})
-
-
-const defaultItems=[item1,item2,item3]
 
 
 const listSchema={
@@ -44,55 +33,26 @@ const listSchema={
 
 const List= mongoose.model("List",listSchema)
 
-// Item.insertMany(defaultItems,function(err){
-//   if(err)
-//   console.log(err)
-//   else
-//   console.log("Successfully saved default items to database");
-// })
 
 
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
 
-// Item.deleteOne({_id:"628d7183559102d3cc8d7397"}, function(err){
-//   if(err)
-//   console.log(err)
-//   else
-//   console.log("Successfully deleted one document")
-// })
 const day = date.getDate();
 
+
+//Routes
 
 app.get("/", function(req, res) {
 
 
   Item.find({}, function(err,foundItems){
-
-    if(foundItems.length==0){
-      Item.insertMany(defaultItems,function(err){
-    if(err)
-    console.log(err)
-    else
-    console.log("Successfully saved default items to database");
-  })
-  res.redirect("/")
-    }
-
-    // console.log(foundItems)
-
-    // console.log(foundItems);
     
     res.render("list", {listTitle: day, newListItems: foundItems});
 
   })
 
-
-
-  // res.render("list", {listTitle: day, newListItems: items});
-
 });
+
 
 app.post("/", function(req, res){
 
@@ -147,72 +107,9 @@ app.post("/", function(req, res){
   })
 
 
-// app.get("/work", function(req,res){
-//   res.render("list", {listTitle: "Work List", newListItems: workItems});
-// });
+  
 
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
-
-
-
-
-// express routing parameters : creating custom route
-
-
-app.get("/:customListName", function(req,res){
-const routeName=  _.capitalize(req.params.customListName);
-
-List.findOne({name:routeName}, function(err,foundList){
-  if(!err){
-    if(!foundList){ 
-    // console.log("Route doesn't exits")
-    // Create a new List 
-    const list= new List({
-      name:routeName,
-      items:defaultItems
-    });
-    
-    list.save()
-
-    res.redirect("/"+routeName);
-    }
-    else{ 
-    // console.log("Route already exits")
-    // Show an existing list 
-
-    res.render("list",{listTitle:foundList.name, newListItems:foundList.items})
-    }
-  }
-});
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
-// app.listen(port);
-
-
-
-
-// app.listen(port, function() {
-//   console.log("Server started successfully");
-// });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started ");
 });
